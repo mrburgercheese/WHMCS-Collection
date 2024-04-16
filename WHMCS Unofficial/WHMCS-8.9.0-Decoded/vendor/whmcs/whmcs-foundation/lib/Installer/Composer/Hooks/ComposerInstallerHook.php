5da6de8f8ed8826636e42597b2801de5cb8d915f
DigiBoxCC <bakhtiyarsierad@gmail.com>
2024-04-17 05:58:03 +0700

    Update Whmcs 8.9
    Update Whmcs 8.9

---
+++ WHMCS Unofficial/WHMCS-8.9.0-Decoded/vendor/whmcs/whmcs-foundation/lib/Installer/Composer/Hooks/ComposerInstallerHook.php
@@ -0,0 +1,85 @@
+<?php //ICB0 72:0 81:1425                                                     ?><?php //00cb7
+// *************************************************************************
+// *                                                                       *
+// * WHMCS - The Complete Client Management, Billing & Support Solution    *
+// * Copyright (c) WHMCS Ltd. All Rights Reserved,                         *
+// * Version:8.9.0 (8.9.0-release.1)                                      *
+// * BuildId:caf50d8.355                                                  *
+// * Build Date:05 Mar 2024                                               *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * Email: info@whmcs.com                                                 *
+// * Website: http://www.whmcs.com                                         *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * This software is furnished under a license and may be used and copied *
+// * only  in  accordance  with  the  terms  of such  license and with the *
+// * inclusion of the above copyright notice.  This software  or any other *
+// * copies thereof may not be provided or otherwise made available to any *
+// * other person.  No title to and  ownership of the  software is  hereby *
+// * transferred.                                                          *
+// *                                                                       *
+// * You may not reverse  engineer, decompile, defeat  license  encryption *
+// * mechanisms, or  disassemble this software product or software product *
+// * license.  WHMCompleteSolution may terminate this license if you don't *
+// * comply with any of the terms and conditions set forth in our end user *
+// * license agreement (EULA).  In such event,  licensee  agrees to return *
+// * licensor  or destroy  all copies of software  upon termination of the *
+// * license.                                                              *
+// *                                                                       *
+// * Please see the EULA file for the full End User License Agreement.     *
+// *                                                                       *
+// *************************************************************************
+if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+?>
+HR+cPtmME+/vof/0zDYkp29Kb41cRnThvpFr+fp8QHhMH6IMKQ124LtbZsXiJAuTNYzrT6jRBcjV
+5w/n3gWhBddF0c2ivTbuTvrLNqBUIj2QYdtPh243RPt9Gp4LNZsWJBmTPduYMzgwWzQ+ssjXROui
+pqjlV7kLdnuL9Xdh97kpzoid1WiUvMmJhHSPcS+qbxBAMJAdkdxdwcpgowhbxkct+wrD18opxTO7
+6OrLBPSYo1Hxc4g0cndR52AcrmRyURA4dfZvHr+9iSpxflle0iYhRW7X8d1DqbGxwsxczDVEBe8p
+Cs17SOj5qyiJJsiFb3DLMcpwIV/lDYNY08CtDiM6LZqc1W8VCiD0CNJBtApdvZDesCNx1iu/ahLg
+vkR5ZRBp8rLSyf3D0SoQ0kuT55BKZdvu/XkyltpfSOWbV1qw82psXJWn2t49ZyOsmmULqa+ds/Aa
+nglDCOfQxMD2/E658/ASyJBgX/mhWsyuygTh/oYA9DY5bokPZM03s5Fqofgad4Xgy6+Lyowi5d/y
+Vpbtm/YrUvKmIFfadfSD/yiwSOC2L+cD5miFKCkv5J5CpiMCH4uSxV1XjUCa3PhLPcD9QuTt/1Qv
+HipvRboGMkzMuu8QYN0+q9YTLhc8D+y33ElB06nvGlTaIb8RGd3hnnzMVBcQH05sEmlQaTtbuqsl
+LV4NwOPUhCPmDVFxGHpRxkRYki2zKvtPIkkTs1rZZWmCTHcK5s7hh2VeOYtMp3vu7OR0bWmimzEs
+5gDuEl/4HIpUvcvqtykO5wuQrKjl9cqXkv45WZOvImARatW67KKvJmXJL2nulffGs53dLDOKqDev
+vf7emLt2tJra76IG04ftFReiuG4Bs0ww5pHNMGmmuRH/dvLr0O7QR15/BILTv+Gx9OmYWci/YA//
+dv23/d0mg3A1/76+GAh30ubcxNO9VQYdBffrAUpA59aJ7YcgRRF5lxmcmVB90iO522mWsjHs4Lc7
+NXolr3DxFoQzPUYbc8LFToS6xGbdjph/CXsOYZYVl7wpKFtmOBtzk+RXLpCS2B+FV/ntgeHNBPcX
+TIHmTxr4PwkEDOPApq4w5fuQ1lxgM1hedwAaPTQR+YKDb5JHcRHvf+2ORQeUMdiY01uWywIgUSvx
+ovLAz3CsFSGevaPViCxOm0TILtgEw6MG7eU1YyIzsyNm7ldGL3AqUTwneQt7Gi3c3RlNutcRB4zT
+ticXT0N8jM5zfgJ+VdeOPB5UMUL+v/q9qvPHdY0voPHJSNl/e2FKfn+3+2AvwpG5PhgoPYa5tbPq
+lpZOmAFcN5NxgSntAAt43yBC14m/zUJryEfgmKcc4R1Ze8M31QhCAAOsxY5Yo0YdObML8V/ZYdji
+GWIsH3dWcX5Z8ForVWILD9ftgKAoLkKSKMa9KaBn4NTFqWsLmE90K33h0rxoZsyMlyoC4ZUZ5fs5
+YHPyMLelRXlDywBCACTsgIBM5UH5jSRc560+bF89o+ehIclrUlUO1wZRJCNVNBrCzLqRXsXGK5Mt
+Uz0RJH4VE+9nALPCR896FfBplsffLdznCj0Gh9hkZ1GJCIAXXrxi71aVqRxAR6m73e9NpyWz/hZE
+WIGwUXAoESb4nOlP4tCOaE16NfNyAS+68p8a7ZxKu0UvNFhyMlm/00Xs1vKXbcu5oCowEFcF337E
+p5POyIS/0jVSw3Exzof+C4UHgjLXPrbFThdWLPE3nk30ryY35kyQE6R54mzbi13b7KPh1CT5fNLF
+EpKIXntlLenEWoiwV4QemMmgCrLBZorrxk7K6m1od6F/vJgeyMaUVMgQ2f+nGclQRgbMeBmJbGOR
+uLtch2sHKTt+6Is+8UECULexA9TujQNRojaXl6IxmJUaPG===
+HR+cPzT0yuKibSUgMVbcfkqLrRuV6zQ5yCqinQt8yWJJbbPp3QVIIErDtaKLQGtS86ZO9xn02PKX
+/B57uGCHJGDck8v6/uh4acR4SayLw/RfR2L7rcc9Unxz58moyh8DMB8QQ4LT/wVKGneBLtaTw/E4
+2uUMeC/BctpcT2/7XnfDHgcVKmiNQC+2JB4XcjD3P3hnYO0d+9Ied1TjC8pyEz8kKXmVuSqwN/N5
+u94mfvxi1z4WQDX9JahMlaHhfkm4VCdHYw7gQ3NLYl9whVf6QVZ/mVtUjUqMvxdWXmOV4mG/ugS/
+AVQST7iIGcaFuf2AZtmyWHorQlIPpr7tUcV8HF5DPP4YqgqX8TQcA8OEEEYTmNhXc7lLYw9FqjYK
+NCOI183JuawXCE4xyVT5V9AAb8xmIiZ45imWD3ljUyVR1uZBfNxg7PQ53N8/eFZSXGijWbnW5yf7
+oES1POn6EgtgeSPQ/Ekx6SXjIZjjeo2qQQrLyPOfOk/v+GyjFR47BxOGJmJm2YSTwLqjOi7PIPhY
+rRVS3mAE9kDfXXvMS6RHi/qb0V4CHk1EEK8byRBdx7dOcp5pvzIhpO+zpnoNCMyMyO9rM3NpEk2y
+cA1voAgzM8AkZ0tiV8YhVjKHrECCPzKEUjBJ69slRquVSTueYiaO2d4F32qCR125Dsvg/n7zLRup
+9tdcDthqw1XrpouBKiGXIfLIKTw1LhGNt1+UNe2LrPfwcbINWolIB+E3qOKpw/Al/5cPRWITIDS7
+I3EMDmqYGjrK8CvuTTy1dLwNzTdcMknpdah9NxI6oAo2VSv9nU4c6bhQTdNPd2o04HfRElLWKwRB
+JfCbfXSJL8pbR57K/A5OjUCrOmMEkprd8Z7huxOAh8WtsZKfR89s8NTDeZiRnNJVwYpHCFSHOowN
+l8SiJmfpONauYpuZRYSzCxcLJrk7Gc+3giiSOz8UskUlmUaz1+4FpHYOz6mobeag6ilGJDctDtXU
+PI4cjqyGwqBB8gfn1X/Y7JA4XysTu4279TZmpFbuKikcK0qmoinaz+RzmuqSjCACJlV3S/R5Dq5/
+nPgrNnl8tseaiN+OXTRKSBbbui2ino/LGKrXZVx5I1c4XY4+7bhzzRSIB94Bw2oKg25SeKXrCuex
+0JIa73ZWxOrhPQVB0qjKbxDhReBnRi/dUnz38XsZzp9s/NlUsHtf0gl6ycHWW7nkTqE51rPYycl+
+0fSXLAoX+3sU924GP4BeFLe/Xus9qZ9HX4neLBWhvJuOBYhSzU7jthsnR+Lm72NyYr+CxQ0IT/nB
+loKgtq2AbCSUoHQxolmkWf8J22kFFkHAguRj49Q7cCWKl8dIvDByG8gKKz/NZHUHdfMWdNSW4WCT
+rTAIBpv84zFFtiX7YioMwKYBDNTzIj8F2tPLuFyg9bWdjU/Qtp9ic37bDiLbk8KrNsd1lnemWlqB
+59c0+tCM9eFs/K3i4X+OgnTNPRzwdXmwic30CF592vDBbpgK0HBMN5Kk5m+Dim1BYKu2UMhhLtBB
+k5M5vNHL96Kfx1kvI8nCvSgxWY9xmzmNmPg3WCAP2xTYoPJJEMBeMryFaHkhiwsIkdtSKd+fuQ52
+l7r1rb4wEZtv6283+Eazl3UO4/faERH3pdPZKd8P/QaDU6A6GlFmBW24E+hPZ0kVuF6zVMBtFR/e
+hDtJA4EA3rr7UgDRkLbemlM5a5hGtqFgo9HRK3VbnYCi8XAJ+TPsFW6paFhmpzQbCxoKuxGgiyLi
+tv+uL9mXNG4R+Dkrf1ZS0G==
---
+++ WHMCS Unofficial/WHMCS-8.9.0-Decoded/vendor/whmcs/whmcs-foundation/lib/Installer/Composer/Hooks/UpdateConfig.php
@@ -0,0 +1,113 @@
+<?php //ICB0 72:0 81:1884                                                     ?><?php //00cb7
+// *************************************************************************
+// *                                                                       *
+// * WHMCS - The Complete Client Management, Billing & Support Solution    *
+// * Copyright (c) WHMCS Ltd. All Rights Reserved,                         *
+// * Version:8.9.0 (8.9.0-release.1)                                      *
+// * BuildId:caf50d8.355                                                  *
+// * Build Date:05 Mar 2024                                               *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * Email: info@whmcs.com                                                 *
+// * Website: http://www.whmcs.com                                         *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * This software is furnished under a license and may be used and copied *
+// * only  in  accordance  with  the  terms  of such  license and with the *
+// * inclusion of the above copyright notice.  This software  or any other *
+// * copies thereof may not be provided or otherwise made available to any *
+// * other person.  No title to and  ownership of the  software is  hereby *
+// * transferred.                                                          *
+// *                                                                       *
+// * You may not reverse  engineer, decompile, defeat  license  encryption *
+// * mechanisms, or  disassemble this software product or software product *
+// * license.  WHMCompleteSolution may terminate this license if you don't *
+// * comply with any of the terms and conditions set forth in our end user *
+// * license agreement (EULA).  In such event,  licensee  agrees to return *
+// * licensor  or destroy  all copies of software  upon termination of the *
+// * license.                                                              *
+// *                                                                       *
+// * Please see the EULA file for the full End User License Agreement.     *
+// *                                                                       *
+// *************************************************************************
+if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+?>
+HR+cPvlUYBlUYt6yrnSTcCNuVg8SzjRy29N2fBB88qxLGsccd4lUOSsJ0BZiO7Ysn/vAEu9kTvwC
+walFwM7USAT2Vsfa1Q65b+QyUjxvhxrYs5PEykuYFuqoPLFboDiPIznAs0M0BRmN//xySqMMIgP3
+cB5vQTaZSn1JMl0enu0ue2bWn3eJKimdFXUQMXN/bANfw+3bKL9zrbA8SpT2RkpuOSwjUhdRQ6IM
+4r6Pjy4ZvRH6P5UYHiMc1Z6jPvhdFrokhmvtRsvKM4EQAKlX5B7cDNnzPN1DqbGxwsxczDVEBe8p
+Cs0ATzRIaCfFP8h4M29L6d3wNV/h+KLD4DmObMp5UEDwEh/QAXjKQpFpTXh2msP4Dm2MpmE4R551
+hw2nsU7COlrz9Wrx/8N39UNvykwnX+cOAjQCMNwZWrK/qNb+Lt563X4czkscE2hs7tiEhCfrzcMy
+ZKxBHoDNZvg37DBKMF7cm/tF7Q0oO0uF2zjut5gkj7TjfrvHAavHDiQ1iGONA5697PXp0VfomRFL
+JTXvbgQccR5YLjPxUK5u5WB5esfJbYkCIlX8FUbwXkAi8nYVPH0AxaXn10jEY0qD5rkVKPFZVnpU
+VUm/1qw9lTrEK7fZ8m1zVd4OehGEI2oxy4hwiqHH6QJ1sbFvYoLJHpUfWZLneo1q/yEqZZ6kebEA
+LGO3/YQ5tZeV1CHGS84qStZ/NPQBNFmiqmksWpEaSoGbjh2P/LdfaC9lo3R6/jJ8V6kGdRu2cPRs
+HuR+Exp8HAEzTXfyC1I6nf3Xb3WGxwBNDBNmhl1DWmNll1FWU0+WLVOaf9q9ciX5R+JTfF1PDc3H
+r7S7V59t4Q88SntcSeq5Fl6S9w/iHFnR/Pio9d0FUstvEH91K9Gs5zg+G2Un65zaCI726rh1G8AV
+yn8kuwvs8ZcWBvpSnd2LYCc379cm/wNISBqoZaVJYgTWis9X2v3vzYdv8B45oReWnscJ/0RXH5r4
+04SrmhLO/pITdFoAP0lDV6p3gZ3y4M4Qc3IetItqOMuqjEK9jzgEebyp/UtG8IlYWtsdLC86HcQi
+jsaZrD6xIKNuzMjmp+wQXssIYOFPuJJ3xHcNvbvb6RMlXT+IlwZokWFah2dvhYXNAfbdNNEkAfma
+xWMQ/uBdG79ZczIYY1RJ7FflH2RCwWDr47YXvlRWg/H4TDjt/juRlr4jseC6xi9inF+0tIkxjlk0
+K8RW/BaG5yo7oKj3w7qRlGtwPNleC6m1lf/DzFPEtix690NVGLsCPIZVMNb16rRqnjh3CBBuqIxM
+If91j7SsSCbA6eZQW5DoV9bZ/FDfra/IwDD3+Q1f/8qVI1IhgvGZURmoknGQaDis0iu4OGGAmuev
+WKSbIynOptecK3/iR6hcryvqu3iagTwgcgFFNIuaPUZH4fkDy38bvZ7o78ewmDpdPtzcIXl8SI36
+kbbeCwO/tqGHHPQpTII/DOP2ySzN1OMQQ574jhiXfFx/ICZgvcg2D7iU0vBLQD/0RxyZ9CTwSsz3
+zDGdIWKuYGDjHDxCio8uq5L7p03Zkq/y3APL+WQ0ptFqfnQA3lLxQyH+EGs/78bJQaw8N7eAI++x
+9fb7FsCRiPe4PL70iCEPL7NtQRAffjm1dDArQhRhQh9mtpdzABe4QKtgdITLgNska473EscyZkMT
+7AtT2Yt7lOFliUohcRCxFbBTl+x3hzn0ZbHqB22S2D3D4WiSgXklFM9x4DujzWJy6D6qOYVX53Zp
+yA3V1lZrW0MgZ3Ly32iXkiQZ/ayxGnDIb0xazR2Aot1u8Su+njB0J48NncjsPVrDccnlCGG3dBoI
+PGpLTllCCrMJM+JACIQooj5ywRRUkHpXtHNIdcGm4z7C/pJ2dKYCZkSDVuenUZAE+fSM7tl3ngDM
+NYoFGosCK7zDjsqHzh2nda/nfLJ1mrWeZ+QRsXNCu5qjbfxqdFX3L3HIdxkLzovqI8zP6vCqCdvM
+utixsj7ddMdRBgCZBguVxraLIAS4oWldeBwyck/7Fa2IrUhp3fwxn+4WcAfAWqdvcvpU3unlWd7X
+fe0Vg00fMORvZHt/YlltphdSFGUso1T8DfbaICS31fXWpkMfU9Rd49yWsI0Zx4Rk9F4YdHIEFqMK
+lyXWf4EF+ZHRF+FOWzubFwOT6x/4dqtJu9HuqS18ceIwDLH0spMLP99msQ8AHC6ND5PlGVP/OIjl
+H/jFmaMeU4AYd857HO9H6YBdP2HW6NgPsbvLCihHcuNOif5fC/8nzcVToF4V80SxQyD8wl8nr6CD
+YqsoP3AOxPsUG1y2ALJpotXbcZkiKlYtXcZeQ3d2eIL0Tg6mHz8SdKDhofqenU67xjfBET3VUHRB
+ulLcDqZnJo9XKWvyUTP3+Uxufa6d+jwyqZ+5kYixozJN2k/ivkNuElzq4ttAMVLxnW0WgfnCq/o0
+FPEdTWM04yW8lMLp/oRuttGtlWbEyfw9XG7ruB5AyDzIsGYRAFEr/508US6lZv9bd0XGaD67V/TP
+2EVbYOf0rMvkHDrpj1cF5SMHM6SUn+Ea1GwHljoxkCmRbZI3hCBawSFw/xzzSPEfiQmj8yhN30Np
+f9hE48M3UsKg0LvXHHPsKdSK1XqXjahcMy68CNtgMYyRuOZkcm/JvHGcEbk65Wl35pIYCvRrW5Fk
+eXHrGKtloAWbIr0bDY85pJiYqVGpyh3Fs9GDQ7Sp8bx8zSOuwCAsz/ZTmXEzEVrs2xoIG6LGxvaj
+FGIKLsbU+94QLLnkeCCZIjugdpGhY24BawYGfM/rzpzUZaMdGuZW0xdlIWv01dBZ0f8ABybxf+r+
+uFikQERwhkg8+9PHPPTef7fW/JvwDEroxOMG5IaBAbOepp4OXBvr4QjcNkU5QeXNG1JI5sSvH4Uw
+7r4TSbN2yjOU3W9Uiha1Z5xcyJkyu+PSfuZjgcDryOebzgm7AEuY5d4bWqi+nxNDVExfHrnCGb5+
+jEwjfUbUy0===
+HR+cPmD6aj7Wh6RcZV87yZIbgUXUxAnCc0rHcvl8nV69FaEEPiQVpKvnwzJJtcTNrUVP5f5LaIxB
+ozd1vd7Lfi5l1pT/l/AgCnq99sfTM/ZK9GXrq5DIzyBYb3tSx+FGHWqFOTEcfcQzqKcFx9GuB02+
+EctYoSxgmvaO9AiUReh87fvBQDVOAf2NTGp0k8VCzWOpL4sxcjwtavdLi09Ibe7hBrgfrwo+JsUL
+OEnJVlb9O7Gp0BqNlZeRl6kb28+HKsoeTN/NKDWK4XSAHYQGn7fPePFtREqMvxdWXmOV4mG/ugS/
+AVQsTNwYC2Xz2ImoQbKyWHor7JjVe/EU5kDCcOfVIlOi8s98JOU3VvApYZWd9U1yw0RsGq8gzC2/
+Lr4ljzts+mL7XoA3LFtdHWxtSBK0bZ41s94LDiA8AKvgxnSAXmWWHRkS/FtuZfjdZMkimmXDiVsK
+AXKFnzGRgiHQSq3ZLlP5ldD24Fhi7T/v53PgdIptQLW3RdBz+omvZpOdJGaYOfq//aZJDm0+aS53
+OUo4JU12wBO5DQ1czdobvZHg0EBTpHwfjKwk+/HQCb9gEbPMQZIJQ7FUwkXr+XmE2vs5jE7KWgHI
+KB4aGezGID3QaT/B8rn/zjYgwTMa32Y/JeW44cWVR6gMa3YIwWE4AdPtYZtW9uYRPya/wq7/6p86
+TWR8fl7sedjKQdC6bRQ7TB/k4EjizZz3qcB9km9KMB4qeoqvZCtO70XqzNtWbUUkeovj6Rc2qZ8l
+XqTREiGIIfmA5KNOOeSf5TGHL4wPX2apxPmmn2/8rtkOn39njjvFDSGzGE0RUFtjV8oB3OE87BHo
+/U3UO+ezM26Jtxflpb0P9lo9Hd7bd7UTXlc23iAw95XpBwzOq5b27WWzp+E3TbKtJEgYNRkveTqK
+WDJpr0xq9qwCgmNy1nBW/iaM/biJiOLiJICvlfGZYeIWhnWtiGAgkPQY4aPG4NxSF/DhJvEJPx2Y
+nc1RIg/UU30lcf7Hw7i3B0hKjzHMeMptH7bzk6Lj5cC/nBvdwq1aP02Px26Cx8qj1wOSyLtitHUp
+9eDuOCjAHNG8YxUUWqIj3GPxjD9dl/NuE50GKxbl2g0bO0a3Cl7/5luojPZ9OvrO0yioMaHb5UtO
+z8fen7brxPuFHvT+AoDZ4JyBJvniScNdMwqTM4SRPkDyX9DXXOOuI4PfmGJ30VuUWB0hXPr8hCPD
+M3x+WXyFceLggwRBGiDY+gpb5RpsNJwVOOdPHNlo59E2WRQus9DF64hynMAY5zoZWmGWBYGSQDGB
+BMMrJ2GeGQrO7kkgvNJPuEwU5PDpCriYBmPUOE9WApRi/BcRGUofC1RlP70h92C5++h/Qti3op5F
+JXJ8WOofWlijqUWo8asndTZutRThcqJNvGQ57iTt1OqJN09JAvw6HRThw+vXqjgFrfqqWsCQ2mnD
+HD4Q70wx2cVseBHupI7PX0dIt8UI7OzFRR1nyfWK1QThrbnT4wNqjT2Dp1sHRIKs9OMFkbSab3Bv
+wDnNQWkVEXIo8mWpMcelOkWLwTUhP6O8/m9FWZICBZklJfHzdCaH+VY2wNUF0D0+9MYP5m5XGmeh
+yyuJk3GOClNKq4Fz3zW6FwpSZ7tbM5LkZKQzRkrG4NO4ExZpRxmThAE2ZUyd0htq8qcAbOGkduM3
+1QpKKcdd55VGSvNzPDBNfftPYcy/Z9GMS9agZQ9uNLHX8SvERX52njizaMvyCHWPj0i9guBJz23K
+5zejMfcPqnRxgb6cLCaFBGdpqztXwYBpueGzmxmtvViDoGs20rg4U5cvuhMmSKCUv2KwunPtApCC
+cvlMVtSce1cOdNWWsoCfm9G42czT526P7ORzkHJ5oSmLqZb4jBAm7fADYAbiUMFX3rCnSl/j4hiK
+quJu6XJhyMarpDuuWLZVmIKcBf+sBPKpBP6A4HwQCX4aHDtk/xkGPHGogFoxgES9xykdYmTGdvIo
+f9e0GSXSQlvYBs6MW8oaYRAAcpqjKCuc3na8NXSfvDBLcxsE3goAARFdARKej6mNto4pmR7X+GcU
+7jofrhfTPJz9TB5XRuxAyRZDe1A726NpqQj972ODz187lgWt56ADEUpBsMb4H+AEtlKXTK6LjUPc
+ZOmTKE3ugMRU2z9zvxoL67XN98mnvHvIcRseHkgkx6B1R7vDT+ww4uCOCMDG1fLZVUWXbZ65BIBa
+QGJFAO5WL0xqdl7hYHScWMtfncgfDIimnfspCYQkj/UqWhUg86/DZn8E+ZvYomWb9So6EXQra16s
+Kk/a9BA4p5Q8ol/MzxU8BuYK0IXDe8ya6dE5VA+Slhs20eWnJ9+D8TXsqE4wym0NuuOFN8eG5fz+
+/dUFa4w18yHEJhJ2Qywx8pQjFM33RvSe+KR/9YvmxakK8RjzJrrbLVyh/rhQztzLQZ12CMW0jw6h
+GuxzugfKzaogXoOh2E3aFml6KS+3NqZZzOfWb5NGl5Em67cx0WSEkuaWLS0mtAYfVufEnBdeJKfA
+4AHEVXCpc4TR1636aDV87xBDId254tXR4FffB+9hTCqmMPaYgjdim6Ay1TeSFMjruHPtR0wu3Nfs
+tjjB0na1grq4lgi9kEcP6Lhg2kz1Pni9rpr8Kvylw3JTDtAIGYxSFpC3UERtZPsNMEYaIfXSShoh
+AN1qn4GGarCUuuRHgGrQO7CYiUf2aqjgqrRYSyRw0tjrjzNUWLK6i9aEs5qiW+dsmtYzoilE/CVr
+947JBig3xU4UlkPnGLqNLMY3QUUwA1m9SiBpm6MdCkCJ41AreKEpOtlvRW==
