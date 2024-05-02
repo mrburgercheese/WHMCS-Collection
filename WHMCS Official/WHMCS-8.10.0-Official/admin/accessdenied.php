098c2e09b076c6502d194870fb7d2e3483167c0a
DigiBoxCC <bakhtiyarsierad@gmail.com>
2024-05-02 08:38:34 +0700

    Update accessdenied.php

--- WHMCS Official/WHMCS-8.10.0-Official/admin/accessdenied.php
+++ WHMCS Official/WHMCS-8.10.0-Official/admin/accessdenied.php
@@ -29,6 +29,7 @@
 // * licensor  or destroy  all copies of software  upon termination of the *
 // * license.                                                              *
 // *                                                                       *
+// * Please see the EULA file for the full End User License Agreement.     *
 // *                                                                       *
 // *************************************************************************
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
